9c4abba5ff891231854c1d8a8f706bc2263d612c
PHAM XUAN THUY <thuypx1983@gmail.com>
2017-01-02 22:25:57 +0700

    shopping cart

--- sites/all/modules/sutunam/product_cart/templates/product-cart-popup.tpl.php
+++ sites/all/modules/sutunam/product_cart/templates/product-cart-popup.tpl.php
@@ -1,5 +1,5 @@
 <?php
-$url_checkout=url('content/votre-demande-de-devis');
+$url_checkout=url('node/21');
 ?>
 <!--<div class="cart-introduction">
     <div class="cart-service">
@@ -9,35 +9,87 @@ $url_checkout=url('content/votre-demande-de-devis');
 </div>-->
 <div class="product-cart-view add_tocart_popup">
      <div class="product-cart-popup">
-        <div class="cart-icon"> <img class="img_scroll" src="<?php print '/sites/all/themes/hueloc/images/icon_poup_cart.png';?>"></div>
+        <div class="cart-icon"> <img class="img_scroll" src="<?php print '/sites/all/themes/bootstrap/images/icon_poup_cart.png';?>"></div>
         <div class="product-cart-popup-title">
-            <span><?php echo t('Votre machine a bien été ajoutée a votre devis')?></span>
+            <span><?php echo t('Sản phẩm của bạn đã được thêm vào giỏ hàng')?></span>
         </div>
-        <ul class="product-cart-lists">
-            <?php
-            foreach ($item_list as $tid=>$items) {
-                $term=taxonomy_term_load($tid);
-                foreach($items as $node){
-                ?>
-                <li class="product-title">
-                   <?php echo $term->name?> &nbsp; - &nbsp;<span><?php echo $node->title?></span>
-                </li>
-            <?php
-                }
-            }
+         <table class="table">
+             <thead>
+             <tr>
+                 <th>Sản phẩm</th>
+                 <th>Số lượng</th>
+                 <th>Thành tiền</th>
+                 <th style="text-align: center">Xóa</th>
+             </tr>
+             </thead>
+             <tbody>
+             <?php
+             foreach ($item_list as $item_id=>$item) {
+                 echo '<tr data-pid="'.$item_id.'">';
+                     ?>
+                     <td align="left">
+                         <?php echo $item['node']->title?>
+                     </td>
+                     <td align="left">
+                         <input data-pid="<?php echo $item_id?>" class="quantity" type="number" data-pid="<?php echo $item_id?>" name="quantity" min="1" max="15" data-price="<?php echo (int)$item['node']->field_price['und'][0]['value'];?>" value="<?php echo $item['quantity']?>">
 
-            ?>
-        </ul>
+                     </td>
+                     <td align="left">
+                         <?php echo number_format($item['node']->field_price['und'][0]['value']*$item['quantity'])?>đ
+                     </td>
+                     <td align="middle">
+                         <i style="cursor: pointer" data-pid="<?php echo $item_id?>" class="fa fa-trash"></i>
+                     </td>
+                 <?php
+             }
+
+             ?>
+             </tbody>
+         </table>
         <div class="cart-checkout">
-            <a class="bnt-continue" href="javascript:void(0)"><?php echo t('CONTINUEZ VOS RECHERCHES')?></a>
-            <a class="btn-view-cart" href="<?php echo $url_checkout?>"><?php echo t('voir votre devis')?></a>
+            <a class="bnt-continue" href="javascript:void(0)"><?php echo t('TIẾP TỤC MUA HÀNG')?></a>
+            <a class="btn-view-cart" href="<?php echo $url_checkout?>"><?php echo t('THANH TOÁN')?></a>
         </div>
     </div>
 </div>
 <script type="text/javascript">
-    jQuery(document).ready(function(){
-        jQuery('.bnt-continue').click(function(){
-            jQuery.fancybox.close();
+
+    (function($){
+        $(document).ready(function(){
+            jQuery('.bnt-continue').click(function(){
+                jQuery.fancybox.close();
+            })
+
+            $('.fa-trash').click(function(){
+                var pdid=$(this).attr("data-pid");
+                var type='product';
+                var row=$(this).parent().parent();
+                row.remove();
+                $.ajax({
+                    url:'<?php echo url('ajax/product/cart/remove')?>',
+                    type:'post',
+                    dataType:'json',
+                    data:{nid:pdid,type:type},
+                    success:function(response){
+
+                    }
+                })
+            })
+            $('.quantity').change(function(){
+                var pdid=$(this).attr("data-pid");
+                var type='product';
+                var quantity=$(this).val();
+                $.ajax({
+                    url:'<?php echo url('ajax/product/cart/change_quantity')?>',
+                    type:'post',
+                    dataType:'json',
+                    data:{pdid:pdid,type:type,quantity:quantity},
+                    success:function(response){
+
+                    }
+                })
+            })
         })
-    })
+
+    })(jQuery)
 </script>
