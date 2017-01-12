176e9e6ec273100967cc980fce1f5228e4d9c2c6
Pham Xuan Thuy <phamxuanthuy@sutunam.com>
2017-01-12 11:38:07 +0700

    meta noindex

--- sites/all/themes/bootstrap/templates/system/html.tpl.php
+++ sites/all/themes/bootstrap/templates/system/html.tpl.php
@@ -56,6 +56,7 @@
 ?><!DOCTYPE html>
 <html<?php print $html_attributes;?><?php print $rdf_namespaces;?>>
 <head>
+    <META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
   <link rel="profile" href="<?php print $grddl_profile; ?>" />
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
