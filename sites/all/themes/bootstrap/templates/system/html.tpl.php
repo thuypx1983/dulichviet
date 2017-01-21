f1addae04b0b4b203faa3c92d13b8b9a3341408f
PHAM XUAN THUY <thuypx1983@gmail.com>
2017-01-21 23:40:35 +0700

    new update

--- sites/all/themes/bootstrap/templates/system/html.tpl.php
+++ sites/all/themes/bootstrap/templates/system/html.tpl.php
@@ -83,7 +83,7 @@
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) return;
       js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8&appId=603747746459920";
+      js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8&appId=1618418868466260";
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
 
