032c1d3ff75fe0ae3267b51909c35590e04b24a8
Pham Xuan Thuy <phamxuanthuy@sutunam.com>
2017-02-09 16:14:14 +0700

    tour detail: table price to responsive

--- sites/all/themes/bootstrap/templates/system/html.tpl.php
+++ sites/all/themes/bootstrap/templates/system/html.tpl.php
@@ -105,6 +105,7 @@
             var type=$('#views-exposed-form-search-tour-page #edit-field-type-value');
             var destination=$('#views-exposed-form-search-tour-page #edit-term-node-tid-depth');
             type.change(function(){
+                alert('kkk');
                 filterDiemDen(type.val());
             })
 
