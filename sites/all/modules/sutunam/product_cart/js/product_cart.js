0f63c83f11b69faebf53ac27a28932d14a5551b5
PHAM XUAN THUY <thuypx1983@gmail.com>
2017-05-14 10:35:44 +0700

    fixed form

--- sites/all/modules/sutunam/product_cart/js/product_cart.js
+++ sites/all/modules/sutunam/product_cart/js/product_cart.js
@@ -9,5 +9,15 @@
             $.fancybox($('#block-webform-client-block-470'));
 
         })
+       $('.btn-product-cart-list').click(function(){
+
+            /*var tour=$(this).attr('tour');
+            var date=$(this).attr('date');
+            $(document).find('#edit-submitted-product').val('Tour: '+tour+ ' - ngày khởi hành: '+ date);*/
+            $.fancybox($('#block-webform-client-block-470'));
+
+        })
+
+
     })
 })(jQuery)
