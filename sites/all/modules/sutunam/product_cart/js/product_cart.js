cb091add611f0b3a8361599b792ec118225e2bb7
PHAM XUAN THUY <thuypx1983@gmail.com>
2017-05-14 10:38:45 +0700

    fixed form

--- sites/all/modules/sutunam/product_cart/js/product_cart.js
+++ sites/all/modules/sutunam/product_cart/js/product_cart.js
@@ -2,15 +2,15 @@
 
     $(function(){
         $('.field-name-field-prices .btn-product-cart').click(function(){
-
+            event.preventDefault();
             var tour=$(this).attr('tour');
             var date=$(this).attr('date');
             $(document).find('#edit-submitted-product').val('Tour: '+tour+ ' - ngày khởi hành: '+ date);
             $.fancybox($('#block-webform-client-block-470'));
 
         })
-       $('.btn-product-cart-list').click(function(){
-
+       $('.btn-product-cart-list').click(function(event){
+           event.preventDefault();
             /*var tour=$(this).attr('tour');
             var date=$(this).attr('date');
             $(document).find('#edit-submitted-product').val('Tour: '+tour+ ' - ngày khởi hành: '+ date);*/
